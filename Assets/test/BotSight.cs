f60eeba2100a9671c700e0db9b76ad1fe8464a64
Ivan-Uspensky <frey.terry@gmnail.com>
2018-12-13 01:05:25 +0200

    isSeen implementation

---
+++ Assets/test/BotSight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(BotState))]
+public class BotSight : MonoBehaviour {
+	float heightMultiplier;
+	float sightDist;
+	float damping;
+	BotState State;
+	Vector3 playerPosition;
+	float playerAngle;
+	public float seenDistance;
+	void Start () {
+		heightMultiplier = 0.25f;
+		sightDist = 10;
+		damping = 6.0f;
+		State = GetComponent<BotState>();
+	}
+	void Update () {
+		RaycastHit hit;
+		Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, transform.forward * sightDist, Color.green);
+		Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.green);
+		Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.green);
+		playerAngle = Vector3.Angle(transform.position - State.GetPlayerPosition(), transform.forward);
+		// Debug.Log(playerAngle);
+		if ((State.GetPlayerPosition() - transform.position).sqrMagnitude <= seenDistance * seenDistance && playerAngle > 130 && playerAngle < 181) {
+			SmoothLook(new Vector3(State.GetPlayerPosition().x, transform.position.y, State.GetPlayerPosition().z));
+			State.SetSeeing(true);
+		} else {
+			State.SetSeeing(false);
+		}
+	}
+	void SmoothLook(Vector3 hitPoint) {
+		Quaternion rotation = Quaternion.LookRotation(hitPoint - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+	}
+}
---
+++ Assets/test/BotState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotState : MonoBehaviour {
+	bool isOffensive;
+	bool isDefensive;
+	bool isHit;
+	bool isGunpointed;
+	bool isMoving;
+	bool isCovered;
+	bool isShooting;
+	bool isSeeing;
+	public Transform Player;
+	void Start () {
+		
+	}
+	public void SetOffensive(bool state) {
+		isOffensive = state;
+	}
+	public bool GetOffensive() {
+		return isOffensive;
+	}
+	public void SetDefensive(bool state) {
+		isDefensive = state;
+	}
+	public bool GetDefensive() {
+		return isDefensive;
+	}
+	public void SetHit(bool state) {
+		isHit = state;
+	}
+	public bool GetHit() {
+		return isHit;
+	}
+	public void SetGunpointed(bool state) {
+		isGunpointed = state;
+	}
+	public bool GetGunpointed() {
+		return isGunpointed;
+	}
+	public void SetMoving(bool state) {
+		isMoving = state;
+	}
+	public bool GetMoving() {
+		return isMoving;
+	}
+	public void SetCovered(bool state) {
+		isCovered = state;
+	}
+	public bool GetCovered() {
+		return isCovered;
+	}
+	public void SetShooting(bool state) {
+		isShooting = state;
+	}
+	public bool GetShooting() {
+		return isShooting;
+	}
+	public void SetSeeing(bool state) {
+		isSeeing = state;
+	}
+	public bool GetSeeing() {
+		return isSeeing;
+	}
+	public Vector3 GetPlayerPosition() {
+		return Player.position;
+	}
+}
